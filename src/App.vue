584a393775bf309af47fdb17d044381474d67ba3
Risto Peterson <you@example.com>
2021-03-11 14:39:09 +0200

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,64 @@
+/* eslint-disable vue/no-side-effects-in-computed-properties */
 <template>
   <Header :totalIncome="state.totalIncome" />
+  <Form :state="state" @add-income="AddIncome" />
+  <IncomeList :state="state" @remove-item="removeItem" />
 </template>
 
 <script>
 import { reactive, computed } from 'vue';
 import Header from './components/Header';
+import Form from './components/Form';
+import IncomeList from './components/IncomeList';
 export default {
- setup () {
-   const state = reactive({
-     income: [{
-       value: 400
-     }, { 
-       value: 500
-     }],
-     totalIncome: computed(() => {
-       let temp = 0;
-
-       if (state.income.length > 0) {
-         for (let i = 0; i < state.income.lenght; i++) {
-           temp += state.income[i].value;
-         }
-       }
-       return temp;
-     })
-   });
-
-   return {
+  setup () {
+    const state = reactive({
+      income: [],
+      sortedIncome: computed(() => {
+        let temp = [];
+        
+        // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+        temp = state.income.sort(function (a, b) {
+          return b.date - a.date;
+        });
+        
+        return temp;
+      }),
+      totalIncome: computed(() => {
+        let temp = 0;
+        if (state.income.length > 0) {
+          for (let i = 0; i < state.income.length; i++) {
+            temp += state.income[i].value;
+          }
+          return temp;
+        } else {
+          return 0;
+        }
+      })
+    });
+    function AddIncome(obj) {
+      let d = obj.date.split("-");
+      let newD = new Date(d[0], d[1], d[2]);
+      state.income = [...state.income, {
+        id: Date.now(),
+        desc: obj.desc,
+        value: parseInt(obj.value),
+        date: newD.getTime()
+      }]
+    }
+    function removeItem(id) {
+      state.income = state.income.filter(v => v.id != id);
+    }
+    // Return template data
+    return {
       Header,
-      state
-   }
- }
+      IncomeList,
+      Form,
+      state,
+      AddIncome,
+      removeItem
+    }
+  }
 }
 </script>
 
@@ -40,8 +69,7 @@ export default {
   box-sizing: border-box;
   font-family: 'Fira Sans', sans-serif;
 }
-
 body {
-  background: #eee;
+  background: #EEE;
 }
 </style>
